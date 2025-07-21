608ece39ec5c7c866a4d0ee396595c20b35ea9d1
Мария <mashka_mal@mail.ru>
2025-07-21 11:19:40 +0300

    implemented: cells configurations
    – dynamic cell height depending on image height
    
    – date formatting
    
    – image generating from internal file

--- FoodForEyesApp/ImagesList/ImagesListCell.swift
+++ FoodForEyesApp/ImagesList/ImagesListCell.swift
@@ -1,5 +1,10 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+
     static var reuseIdentifier = "ImagesListCell"
 }
