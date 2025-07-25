61fb6cc70d449efa1879615f851ee1613d04c77b
Мария <mashka_mal@mail.ru>
2025-07-25 09:45:06 +0300

    added: constraints for profile view

--- FoodForEyesApp/Profile/ProfileViewController.swift
+++ FoodForEyesApp/Profile/ProfileViewController.swift
@@ -2,8 +2,16 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet private var userNameLabel: UILabel!
+    @IBOutlet private var avatarImage: UIImageView!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var helloWorldLabel: UILabel!
+    @IBOutlet private var userLoginLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func didTapLogoutButton() {
     }
     
 }
